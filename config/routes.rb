a8e351e20d4c98b8168f68e66bfa9035a79ef484
Pawan <pawan.ghising@mnsu.edu>
2019-04-29 05:58:04 +0000

    use can create post

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,21 @@ Rails.application.routes.draw do
   
   devise_for :users 
   devise_scope :user do  
-   get '/users/sign_out' => 'devise/sessions#destroy'  
+    get '/users/sign_out' => 'devise/sessions#destroy'  
   
 end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'posts#homepage', as:'home'
 
-  # Example of regular route:
+
   get 'about' => 'pages#about', as: 'about'
  
+  get 'rental2' => 'pages#listfindapt', as: 'rental2'
   
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  get 'index' => 'posts#index', as: 'apartment'
+ 
   resources :posts 
+  
 
   # Example resource route with options:
   #   resources :products do
